1eaf502540b5900dde1c710d5b56d7df2ad04c99
Quazar8 <barcode12d@gmail.com>
2020-11-07 17:05:15 +0200

    Moving the server to a seperate folder

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const express = require('express')
-const port = 3000
-
-const app = express()
-
-app.get('/', (req, res) => {
-    res.send('Hello from Express!')
-})
-
-app.listen(port, () => {
-    console.log(`Express server listening on ${port}`)
-})
+require('./server/server')
