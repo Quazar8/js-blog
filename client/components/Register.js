5fe171f20cdb1cdb98411ce927feb50ae1eff445
Quazar8 <barcode12d@gmail.com>
2020-11-08 18:14:29 +0200

    Markup for the register component

--- client/components/Register.js
+++ client/components/Register.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Register = () => {
     return (
-        <h2>Register Component</h2>
+        <form>
+            <div className="field-container">
+                <label for="username">Username:</label>
+                <input type="text" id="username" />
+            </div>
+            <div className="field-container">
+                <label for="pass">Password:</label>
+                <input type="text" id="pass" /> 
+            </div>
+            <input type="submit" value="Register"/>
+        </form>
     )
 }
 
