c1985874d8debf8f4c27f4f8b6791b10af61100b
Quazar8 <barcode12d@gmail.com>
2020-11-11 19:20:25 +0200

    Setting up passport.js and login functionality

--- server/login.js
+++ server/login.js
@@ -1,6 +1,28 @@
-const loginUser = (req, res) => {
-       console.log('login', req.body)
-       res.status(200).send({error: false}) 
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+
+passport.use(new LocalStrategy((username, password, done) => {
+    const db = require('./db.json')
+    const users = db.Users
+
+    if(!users[username]) 
+        return done(null, false, 
+            { message: "User doesn't exist"})
+    
+    if(users[username].password !== password)
+        return done(null, false, {message: 'Incorrect password'})
+
+    return done(null, username)
+}))
+
+const loginUser = (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if(err) return next(err)
+        if(!user) 
+            return res.send({ error: true, 
+                errorMsg: 'Username or Password incorrect' })
+        return res.status(200).send({error: false, user})
+    })(req, res, next)
 }
 
 module.exports = loginUser
