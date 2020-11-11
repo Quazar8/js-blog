fd5d25ebeee63f80f93cf69f4f2ae4abe03a3656
Quazar8 <barcode12d@gmail.com>
2020-11-11 18:13:20 +0200

    Moduling the functionality of registering a user

---
+++ server/register.js
@@ -0,0 +1,32 @@
+const { writeDb } = require('./db')
+
+const registerUserDb = async ({ username, password}) => {
+    const jsonDb = require('./db.json')
+    if(jsonDb.Users[username]) 
+        return { error: true, errorMsg: 'User already exists!'}
+
+    jsonDb.Users[username] =  { password }
+
+    const str = JSON.stringify(jsonDb)
+    let result = await writeDb(str)
+    return result
+} 
+const registerUser = async (req, res) => {
+    console.log('register', req.body)
+    const data = req.body
+    if(!data && !data.username && !data.password){
+        res.status(500).send({ error: true, 
+            errorMsg: 'User fields missing' })
+        return;
+    }
+        
+    let result = await registerUserDb(data)
+    if(result.error)
+        res.status(500).send({error: true, 
+                    errorMsg: result.errorMsg})
+    else 
+        res.status(200).send({error: false,
+                msg: "User registered"})
+}
+
+module.exports = registerUser
--- server/routes.js
+++ server/routes.js
@@ -1,39 +1,11 @@
-const { writeDb } = require('./db')
-
-const registerUser = async ({ username, password}) => {
-    const jsonDb = require('./db.json')
-    if(jsonDb.Users[username]) 
-        return { error: true, errorMsg: 'User already exists!'}
-
-    jsonDb.Users[username] =  { password }
-
-    const str = JSON.stringify(jsonDb)
-    let result = await writeDb(str)
-    return result
-} 
+const registerUser = require('./register')
 
 const routes = (app) => {
     app.get('/', (req, res) => {
         res.send('Hello from Express!')
     }) 
    
-    app.post('/user/register',async (req, res) => {
-        console.log('register', req.body)
-        const data = req.body
-        if(!data && !data.username && !data.password){
-            res.status(500).send({ error: true, 
-                errorMsg: 'User fields missing' })
-            return;
-        }
-           
-        let result = await registerUser(data)
-        if(result.error)
-            res.status(500).send({error: true, 
-                        errorMsg: result.errorMsg})
-        else 
-            res.status(200).send({error: false,
-                 msg: "User registered"})
-    })
+   app.post('/user/register', registerUser)
 
    app.post('/user/login', (req, res) => {
        console.log('login', req.body)
