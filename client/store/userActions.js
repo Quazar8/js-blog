f9efab5e7a8298e0d621c0279c6445000e7c844b
Quazar8 <barcode12d@gmail.com>
2020-11-12 13:48:42 +0200

    Setting up redux user action types

---
+++ client/store/userActions.js
@@ -0,0 +1,10 @@
+const actionTypes = {
+    LOGIN_USER: 'LOGIN_USER',
+    LOGGEDIN_USER: 'LOGGEDIN_USER',
+    REGISTER_USER: 'REGISTER_USER',
+    REGISTERED_USER: 'REGISTERED_USER'
+}
+
+export {
+    actionTypes
+}
