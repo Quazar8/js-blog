063699fcf8de2d13f22d28a24121d977727c62dc
Quazar8 <barcode12d@gmail.com>
2020-11-15 18:04:39 +0200

    Initializing redux reducer for global events

---
+++ client/store/globalReducer.js
@@ -0,0 +1,11 @@
+const globalState = {
+    error: ""
+}
+
+const globalReducer = (state = globalState, action) => {
+    switch(action.type){
+        default: return state
+    }
+}
+
+export default globalReducer
