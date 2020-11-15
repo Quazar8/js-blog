e099af206853024b8d050ec2488377b4be1d040e
Quazar8 <barcode12d@gmail.com>
2020-11-15 19:54:23 +0200

    Handling error action type in global reducer

--- client/store/globalReducer.js
+++ client/store/globalReducer.js
@@ -1,9 +1,20 @@
+import { types } from './globalActions'
+
 const globalState = {
     error: ""
 }
 
+const setErrorState = (state, errorMsg) => {
+    return {
+        ...state,
+        error: errorMsg
+    }
+}
+
 const globalReducer = (state = globalState, action) => {
     switch(action.type){
+        case types.ERROR:
+            return setErrorState(state, action.payload)
         default: return state
     }
 }
