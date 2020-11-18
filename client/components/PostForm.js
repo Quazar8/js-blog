ddc79c58a11b636246c9ede1c5a03ea9db798c78
Quazar8 <barcode12d@gmail.com>
2020-11-18 21:25:20 +0200

    Adding state to the article form

--- client/components/PostForm.js
+++ client/components/PostForm.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const PostForm = () => {
+    const [inputs, setInputs] = useState({
+        title: '',
+        content: ''
+    })
+
+    const handleTitleCHange = (e) => {
+       setInputs({
+           ...inputs,
+           title: e.target.value
+       })
+    }
+    
+    const handleContentCHange = (e) => {
+       setInputs({
+           ...inputs,
+           content: e.target.value
+       })
+    }
+
     return (
         <form>
             <div className = "field-container">
                 <label for = "title">Title</label>
-                <input id = "title" value = "" />
+                <input id = "title" value = "" 
+                onChange = { handleTitleCHange }/>
             </div>
             <div>
-                <textarea cols = "20" rows = "10"></textarea>
+                <textarea cols = "20" rows = "10"
+                onChange = { handleContentCHange }></textarea>
             </div>
             <input type = "submit" value = "Publish" />
         </form>
