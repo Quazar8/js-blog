6c29293d414173ea06197ba48270a02ca1c8fd47
Quazar8 <barcode12d@gmail.com>
2020-11-18 19:57:41 +0200

    Adding fields to the article form view

--- client/components/PostForm.js
+++ client/components/PostForm.js
@@ -3,7 +3,14 @@ import React from 'react'
 const PostForm = () => {
     return (
         <form>
-            This is a post form
+            <div className = "field-container">
+                <label for = "title">Title</label>
+                <input id = "title" value = "" />
+            </div>
+            <div>
+                <textarea cols = "20" rows = "10"></textarea>
+            </div>
+            <input type = "submit" value = "Publish" />
         </form>
     )
 }
