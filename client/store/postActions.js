ba6c9ae73a6a5e45cb13ad380c4c62b8cafec800
Quazar8 <barcode12d@gmail.com>
2020-11-19 19:35:46 +0200

    Publishing posts action creator

--- client/store/postActions.js
+++ client/store/postActions.js
@@ -2,6 +2,14 @@ const types = {
     PUBLISH_POST: 'PUBLISH_POST'
 }
 
+const publishPostAction = (data) => {
+    return {
+        type: types.PUBLISH_POST,
+        payload: data
+    }
+}
+
 export {
-    types
+    types,
+    publishPostAction
 }
