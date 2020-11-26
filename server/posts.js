b3a729afae2aadbfecd4953c1f9fbce8b3ef3618
Quazar8 <barcode12d@gmail.com>
2020-11-26 23:11:00 +0200

    Server API for creating articles in the database

--- server/posts.js
+++ server/posts.js
@@ -1,4 +1,6 @@
-const { genId } = require('./utils')
+const { genId, getDate } = require('./utils')
+const { writeDb } = require('./db');
+const { json } = require('express');
 const idBytes = 8;
 
 const postArticle = (req, res) => {
@@ -9,8 +11,31 @@ const postArticle = (req, res) => {
    } 
 
    const db = require('./db.json')
-   const postId = genId(idBytes)
+   
    const userId = req.user
+   const postId = genId(idBytes)
+
+   const post = {
+       postId,
+       title,
+       content,
+       authorId: userId,
+       date: getDate()
+   }
+
+   db.Users[userId].posts.push(postId)
+   db.Posts[postId] = post
+
+   const newDb = JSON.stringify(db);
+   writeDb(newDb).then(result => {
+       if (result.err) {
+            return res.status(500).send( {error: true,
+                                  errorMsg: result.errorMsg })
+       }
+
+       return res.status(200).send( { error: false,
+                                msg: 'Successfully published' })
+   })
 }
 
 module.exports = {
