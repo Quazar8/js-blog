e2a56ee349f37a5aab1e48a54e52a205b8237f79
Quazar8 <barcode12d@gmail.com>
2020-11-26 22:47:34 +0200

    Method to format date into a custom string

--- server/posts.js
+++ server/posts.js
@@ -1,4 +1,5 @@
 const { genId } = require('./utils')
+const idBytes = 8;
 
 const postArticle = (req, res) => {
    const { title, content } = req.body
@@ -6,6 +7,10 @@ const postArticle = (req, res) => {
        return res.status(403).send( { error: true, 
                             errorMsg: 'No title or article body' })
    } 
+
+   const db = require('./db.json')
+   const postId = genId(idBytes)
+   const userId = req.user
 }
 
 module.exports = {
--- server/utils.js
+++ server/utils.js
@@ -4,6 +4,21 @@ const genId = ( numberOfBytes ) => {
     return crypto.randomBytes(numberOfBytes).toString('hex')
 }
 
+const getDate = () => {
+    const date = new Date()
+
+    const day =  date.getDate()
+    const month = date.getMonth()
+    const year = date.getFullYear()
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
+    const timezone = date.getTimezoneOffset()
+
+    return `${timezone} ${hours}:${minutes}:${seconds} ${day}-${month}-${year}`
+}
+
 module.exports = {
-    genId
+    genId,
+    getDate
 }
