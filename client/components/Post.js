124f40b0f2a2ed0872bcaa78630b49359e9d30fb
Quazar8 <barcode12d@gmail.com>
2020-11-28 14:57:56 +0200

    An initial component for holding a single post

---
+++ client/components/Post.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Post = (props) => {
+    const { title, content, author } = props
+    return (
+        <article className = "single-post">
+            <h2>{ title }</h2>
+            <h3>by { author }</h3>
+            <p>{ content }</p>
+        </article>
+    )
+}
+
+export default Post
