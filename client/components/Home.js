5bfa3f60e19abdc7906b46c774f176842722c559
Quazar8 <barcode12d@gmail.com>
2020-11-28 16:59:25 +0200

    Appending posts container to the home component

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import PostsContainer from './PostsContainer'
 
 const Home = () => {
     return (
-        <h1>Home component</h1>
+        <section className = "home-container">
+            <h1>Home component</h1>
+            <PostsContainer />
+        </section>
     )
 }
 
