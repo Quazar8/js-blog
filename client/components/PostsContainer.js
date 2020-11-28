2a20c7c51f7e8f5a23de6a0a809328430db4b8b4
Quazar8 <barcode12d@gmail.com>
2020-11-28 17:12:05 +0200

    Turning posts container into a smart component

--- client/components/PostsContainer.js
+++ client/components/PostsContainer.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const PostsContainer = () => {
+import { getAllPosts } from '../store/postsActions'
+
+const PostsContainerView = ( { getPosts }) => {
+    getPosts()
     return (
         <section className = "posts-container">
             <h1>Post container</h1>
         </section>
     )
 }
+
+const mapDispatch = dispatch => {
+    return {
+        getPosts: () => {
+            dispatch(getAllPosts())
+        }
+    }
+}
+
+const PostsContainer = connect(null, mapDispatch)(PostsContainerView)
 
 export default PostsContainer
