be2f10c9ecd9b015dd998d29e4abfcde239724ad
Quazar8 <barcode12d@gmail.com>
2020-11-28 16:41:18 +0200

    Initial react component for displaying all posts

---
+++ client/components/PostsContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostsContainer = () => {
+    return (
+        <section className = "posts-container">
+            <h1>Post container</h1>
+        </section>
+    )
+}
+
+export default PostsContainer
