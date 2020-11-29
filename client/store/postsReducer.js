2cb9c8a00e062bd548248af87e1ca8d1efb8fbb3
Quazar8 <barcode12d@gmail.com>
2020-11-29 21:01:28 +0200

    Handling getting all posts in posts reducer #2

--- client/store/postsReducer.js
+++ client/store/postsReducer.js
@@ -1,9 +1,20 @@
+import { types } from './postsActions'
+
 const initial = {
     allPosts: []
 }
 
+const allPostsState = (state, posts) => {
+    return {
+        ...state,
+        allPosts: posts
+    }
+}
+
 const postReducer = (state = initial, action) => {
     switch (action.payload) {
+        case types.GOT_ALL_POSTS:
+            return allPostsState(action.payload)
         default: return state
     }
 }
