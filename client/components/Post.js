4d593b6d45553b2661cd2585ce942c1381be3575
Quazar8 <barcode12d@gmail.com>
2020-11-30 15:52:17 +0200

    Changing post component props to get the author of the post

--- client/components/Post.js
+++ client/components/Post.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Post = ({ post }) => {
-    const { title, content, author } = post
+    const { title, content, authorId } = post
     return (
         <article className = "single-post">
             <h2>{ title }</h2>
-            <h3>by { author }</h3>
+            <h3>by { authorId }</h3>
             <p>{ content }</p>
         </article>
     )
