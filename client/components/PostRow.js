d61a81dfc22a1b1973294f0f97e9bd10281f4476
Quazar8 <barcode12d@gmail.com>
2020-12-02 23:35:14 +0200

    Changing post row class name

--- client/components/PostRow.js
+++ client/components/PostRow.js
@@ -3,7 +3,7 @@ import React from 'react'
 const PostRow = ({ post }) => {
     const { title, content, authorId } = post
     return (
-        <article className = "single-post">
+        <article className = "post-row">
             <h2>{ title }</h2>
             <h3>by { authorId }</h3>
             <p>{ content }</p>
