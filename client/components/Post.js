5432bdde5acb331777224e2abc46b01f0944e260
Quazar8 <barcode12d@gmail.com>
2020-12-03 22:13:47 +0200

    Post view component to use post info from props

--- client/components/Post.js
+++ client/components/Post.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const Post = () => {
+const Post = ({ post }) => {
+    const {title, authorId, content} = post
     return (
-        <h2>This is a post</h2>
+        <article className = "post-view">
+            <h2>{ title }</h2>
+            <h3>by { authorId }</h3>
+            <p>{ content }</p>
+        </article>
     )
 }
 
