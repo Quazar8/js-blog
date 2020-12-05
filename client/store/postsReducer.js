e0cc63ddb0a2b03bdf7e93d54e5ed0ede74982e0
Quazar8 <barcode12d@gmail.com>
2020-12-05 20:40:30 +0200

    Changing post's store state on getting an article

--- client/store/postsReducer.js
+++ client/store/postsReducer.js
@@ -12,10 +12,19 @@ const allPostsState = (state, posts) => {
     }
 }
 
+const singlePostState = (state, post) => {
+    return {
+        ...state,
+        singlePost: post
+    }
+}
+
 const postReducer = (state = initial, action) => {
     switch (action.type) {
         case types.GOT_ALL_POSTS:
             return allPostsState(state, action.payload)
+        case types.GOT_POST:
+            return singlePostState(state, action.payload)
         default: return state
     }
 }
