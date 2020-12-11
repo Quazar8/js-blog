034fd1c895128d025c5f0ca0040c6163b73eaf05
Quazar8 <barcode12d@gmail.com>
2020-12-11 21:23:13 +0200

    Fitting the tumbnail into the post rows at homepage

--- client/components/PostRow.js
+++ client/components/PostRow.js
@@ -18,7 +18,9 @@ const PostRow = ({ post }) => {
     return (
         <Link onClick = { savePostToLocal } to = { linkQuery }>
             <article className = "post-row">
-                <img src={ thumbnail } alt="post's thumbnail" />
+                <div className= "thumbnail-container">
+                    <img src={ thumbnail } alt="post's thumbnail" />
+                </div>
                 <h2>{ title }</h2>
                 <h3>by { authorId }</h3>
                 <p>{ content }</p>
