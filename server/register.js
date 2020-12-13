cfead59d71a7972b5a55276b8c816ad7b22c6b32
Quazar8 <barcode12d@gmail.com>
2020-12-13 20:37:13 +0200

    Adding profile picture attribute to db user model

--- server/register.js
+++ server/register.js
@@ -1,11 +1,17 @@
 const { writeDb } = require('./db')
 
-const registerUserDb = async ({ username, password}) => {
+const registerUserDb = async ({ username, password }) => {
     const jsonDb = require('./db.json')
-    if(jsonDb.Users[username]) 
+    if (jsonDb.Users[username]) {
         return { error: true, errorMsg: 'User already exists!'}
+    }
 
-    jsonDb.Users[username] =  { password, posts: [] }
+    const user = {
+        password,
+        posts: [],
+        profilePic: ''
+    }
+    jsonDb.Users[username] = user
 
     const str = JSON.stringify(jsonDb)
     let result = await writeDb(str)
@@ -15,14 +21,14 @@ const registerUserDb = async ({ username, password}) => {
 const registerUser = async (req, res) => {
     console.log('register', req.body)
     const data = req.body
-    if(!data && !data.username && !data.password){
+    if (!data && !data.username && !data.password){
         res.status(403).send({ error: true, 
             errorMsg: 'User fields missing' })
         return;
     }
         
     let result = await registerUserDb(data)
-    if(result.error) {
+    if (result.error) {
         res.status(403).send(
             {error: true, errorMsg: result.errorMsg
         })
