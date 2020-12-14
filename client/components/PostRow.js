b085dc376bfa5e15363fded8e22954c7dd4022fa
Quazar8 <barcode12d@gmail.com>
2020-12-14 23:10:47 +0200

    Positioning previews at center and margins fix

--- client/components/PostRow.js
+++ client/components/PostRow.js
@@ -27,18 +27,18 @@ const PostRow = ({ post, index, postsLength }) => {
     }
 
     return (
-        <Link onClick = { savePostToLocal } to = { linkQuery }>
-            <article className = "post-row" style = { animationStyle }>
-                <div className = "thumbnail-container">
-                    <img src = { thumbnail } alt = "post's thumbnail" />
-                </div>
-                <div className = "post-info">
-                    <h2>{ title }</h2>
-                    <h3>by { authorId }</h3>
-                    <p>{ contentPreview }</p>
-                </div>
-            </article>
-        </Link>
+        <article className = "post-row" style = { animationStyle }>
+            <Link onClick = { savePostToLocal } to = { linkQuery }>
+                    <div className = "thumbnail-container">
+                        <img src = { thumbnail } alt = "post's thumbnail" />
+                    </div>
+                    <div className = "post-info">
+                        <h2>{ title }</h2>
+                        <h3>by { authorId }</h3>
+                        <p>{ contentPreview }</p>
+                    </div>
+            </Link>
+        </article>
     )
 }
 
