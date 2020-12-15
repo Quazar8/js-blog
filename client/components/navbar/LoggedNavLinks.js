c83ee634031b8d3f3311c912e1db5ed79d052d2c
Quazar8 <barcode12d@gmail.com>
2020-12-15 17:14:35 +0200

    Turning user nav link into a seperate component

--- client/components/navbar/LoggedNavLinks.js
+++ client/components/navbar/LoggedNavLinks.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import CreatePostLink from './CreatePostLink'
+import UserLink from './UserLink'
 
-const LoggedNavLinks = ( { username,
-                            tryLogoutUser }) => {
+const LoggedNavLinks = 
+        ({ username, tryLogoutUser }) => {
     return (
         <div className="links-container">
             <CreatePostLink />
-            <h3>Hi, { username }</h3>
+            <UserLink username = { username } />
             <h3 onClick= { tryLogoutUser }>Log out</h3>
         </div>
     )
---
+++ client/components/navbar/UserLink.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserLink = ({ username }) => {
+    return (
+            <h3>Hi, { username }</h3>
+    )
+}
+
+export default UserLink
