13476a5e49d0cac029999214e64feaddbef12b10
Quazar8 <barcode12d@gmail.com>
2020-12-16 20:00:31 +0200

    Appending initial component to user nav link

---
+++ client/components/navbar/UserBlock.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const UserBlock = () => {
+    return (
+        <div className="user-block">
+            User block
+        </div>
+    )
+}
+
+export default UserBlock
--- client/components/navbar/UserLink.js
+++ client/components/navbar/UserLink.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
+import UserBlock from './UserBlock'
+
 const UserLink = ({ username, profilePic }) => {
     return (
             <div className = "nav-links user-nav-link">
                 <div className = "image-container">
                     <img src= { profilePic } alt="user picture" />
                 </div>
+                <UserBlock />
             </div>
     )
 }
