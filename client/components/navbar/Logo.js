eca590a2d85822ade65bf9fde9a9cc4b3932b426
Quazar8 <barcode12d@gmail.com>
2020-12-17 15:30:41 +0200

    Changing Logo stroke width to be readable by react

--- client/components/navbar/Logo.js
+++ client/components/navbar/Logo.js
@@ -5,12 +5,12 @@ const Logo = () => {
         <svg viewBox="0 0 100 100" width="100%">
             <circle cx="50" cy="50" r="8" fill="lightblue" />
             <ellipse cx="50" cy="50" rx="45" ry="17" stroke="lightblue"
-                    fill="none" stroke-width="3.5"/>
+                    fill="none" strokeWidth="3.5"/>
             <ellipse cx="50" cy="50" rx="45" ry="17" stroke="lightblue"
-                    fill="none" stroke-width="3.5"
+                    fill="none" strokeWidth="3.5"
                     transform="rotate(60 50 50)"/>
             <ellipse cx="50" cy="50" rx="45" ry="17" stroke="lightblue"
-                    fill="none" stroke-width="3.5"
+                    fill="none" strokeWidth="3.5"
                     transform="rotate(120 50 50)"/>
         </svg>
     )
