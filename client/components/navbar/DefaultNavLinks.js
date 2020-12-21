ad1d2055ed3c4c1546cdf43aba8a5efc27c1a859
Quazar8 <barcode12d@gmail.com>
2020-12-21 21:22:20 +0200

    Default user menu icon on smaller screens

--- client/components/navbar/DefaultNavLinks.js
+++ client/components/navbar/DefaultNavLinks.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import DefaultUserMenu from './DefaultUserMenu'
+
 const DefaultNavLinks = () => {
     return (
-        <div className="links-container">
-            <Link className="nav-links" to="/login">Login</Link>
-            <Link className="nav-links" to="/register">Register</Link>
+        <div className = "links-container">
+            <DefaultUserMenu />
+            <div className = "default-user-links">
+                <Link className = "nav-links" to = "/login">Login</Link>
+                <Link className = "nav-links" to = "/register">Register</Link>
+            </div>
         </div>
     )
 }
---
+++ client/components/navbar/DefaultUserMenu.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const DefaultUserMenu = () => {
+    return (
+        <div className = "default-user-menu">
+            <svg viewBox = "0 0 50 50">
+                <path d = {
+                   `M 10 45
+                    c0,0 0,-20 15,-20
+                    M 40 45
+                    c0,0 0,-20 -15,-20` 
+                }/>
+                <polygon points = "11,44 25,26 39,44"/>
+                <circle cx="25" cy="13" r="7"/>
+            </svg>
+        </div>
+    )
+}
+
+export default DefaultUserMenu
