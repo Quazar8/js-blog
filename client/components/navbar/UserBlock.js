271544a6e0c97e360ad83886ff710095d3aca56c
Quazar8 <barcode12d@gmail.com>
2020-12-22 18:23:35 +0200

    Substituting user block triangle with an svg one

--- client/components/navbar/UserBlock.js
+++ client/components/navbar/UserBlock.js
@@ -8,7 +8,11 @@ const UserBlock =
     }
     
     return (
-        <div  className="user-block">
+        <div className="user-block">
+            <svg className = "triangle" viewBox = "0 0 50 50">
+                <polygon className = "outer-triangle" points = "25,10 0,40 50,40" />
+                <polygon className = "inner-triangle" points = "25,13 3,40 48,40" />
+            </svg>
             <h4>{ username }</h4>
             <ul onMouseDown = {() => { changeBlur(false) }}>
                 <Link className = "user-block-create-post" 
