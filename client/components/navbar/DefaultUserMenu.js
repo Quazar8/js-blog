d5030332d44d6cca370c97d788a8fdfab4d09e2f
Quazar8 <barcode12d@gmail.com>
2020-12-23 21:09:31 +0200

    Toggling default user menu on icon click

--- client/components/navbar/DefaultUserMenu.js
+++ client/components/navbar/DefaultUserMenu.js
@@ -1,23 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom";
 
 import UserSvg from '../svgs/UserSvg'
 import Triangle from '../svgs/Triangle'
 
 const DefaultUserMenu = () => {
+    const [showMenu, setShowMenu] = useState(false)
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu)
+    }
+
     return (
         <div className = "default-user-menu-container">
-            <div className = "default-user-menu">
+            <div onClick = { toggleMenu } className = "default-user-menu">
                 <UserSvg />
-                <ul>
-                    <Triangle />
-                    <Link to = "/login">
-                        <li>Login</li>
-                    </Link>
-                    <Link to = "/register">
-                        <li>Register</li>
-                    </Link>
-                </ul>
+                { showMenu 
+                  ? <ul>
+                        <Triangle />
+                        <Link to = "/login">
+                            <li>Login</li>
+                        </Link>
+                        <Link to = "/register">
+                            <li>Register</li>
+                        </Link>
+                    </ul>
+                    : null
+                }   
             </div>
         </div>
     )
