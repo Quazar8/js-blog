1babcfb9786a8501ad82dc5490cf7cc4eeffb694
Quazar8 <barcode12d@gmail.com>
2020-12-23 22:00:03 +0200

    Post previews at home page with transition effect on mouse hover

--- client/components/PostRow.js
+++ client/components/PostRow.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 const PostRow = ({ post, index, postsLength }) => {
     const { title, content, 
             authorId, postId,
             thumbnail } = post
+
+    const [postInfo, setPostInfo] = useState({
+        titlePreview: '',
+        contentPreview: ''
+    })
+
+    useEffect(() => {
+        let titlePreview = title
+        if (title.length > 34) {
+            titlePreview = title.substring(0, 34) + '...'
+        }
+
+        const contentPreview = content.substring(0, 168) + '...'
+        setPostInfo({ titlePreview, contentPreview })
+    }, [])
 
     const getUrlTitle = () => {
         const urlTitle = title.replace(' ', '-')
@@ -15,12 +30,6 @@ const PostRow = ({ post, index, postsLength }) => {
         return urlTitle + '-' + postId
     }
     
-    let titlePreview = title
-    if (title.length > 34) {
-        titlePreview = title.substring(0, 34) + '...'
-    }
-
-    const contentPreview = content.substring(0, 168) + '...'
     const urlTitle = getUrlTitle()
     const linkQuery =  {
         pathname: '/post/' + urlTitle,
@@ -35,16 +44,20 @@ const PostRow = ({ post, index, postsLength }) => {
         zIndex: postsLength - index
     }
 
+    const changePostInfo = () => {
+
+    }
+
     return (
-        <article className = "post-row" style = { animationStyle }>
+        <article onMouseEnter = { changePostInfo } className = "post-row" style = { animationStyle }>
             <Link to = { linkQuery }>
                     <div className = "thumbnail-container">
                         <img src = { thumbnail } alt = "post's thumbnail" />
                     </div>
                     <div className = "post-info">
-                        <h2>{ titlePreview }</h2>
+                        <h2>{ postInfo.titlePreview }</h2>
                         <h3>by { authorId }</h3>
-                        <p>{ contentPreview }</p>
+                        <p>{ postInfo.contentPreview }</p>
                     </div>
             </Link>
         </article>
