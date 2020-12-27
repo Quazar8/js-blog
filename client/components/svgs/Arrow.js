0a7f5d03c0887aec5a69be77f0cc3f295362124b
Quazar8 <barcode12d@gmail.com>
2020-12-27 20:44:44 +0200

    Adjusting the line coordinates in the arrow svg

--- client/components/svgs/Arrow.js
+++ client/components/svgs/Arrow.js
@@ -2,9 +2,9 @@ import React from 'react'
 const Arrow = () => {
     return (
         <svg className = "arrow" viewBox = "0 0 100 50">
-            <line x1 = "0" y1 = "25" x2 = "100" y2 = "25" />
-            <line x1 = "70" y1 = "0" x2 = "100" y2 = "25" /> 
-            <line x1 = "70" y1 = "50" x2 = "100" y2 = "25" /> 
+            <line x1 = "10" y1 = "25" x2 = "85" y2 = "25" />
+            <line x1 = "60" y1 = "5" x2 = "90" y2 = "25" /> 
+            <line x1 = "60" y1 = "45" x2 = "90" y2 = "25" /> 
         </svg>
     )
 }
