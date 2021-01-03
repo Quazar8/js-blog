f50943a3822bb8f9ea338f01af3e5f12f3b81ea6
Quazar8 <barcode12d@gmail.com>
2021-01-03 23:22:06 +0200

    Displaying text in profile pafe is there is no user info

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
-const Profile = () => {
+const Profile = ({ user }) => {
+    if (!user) {
+        return <h1>Can not display profile page</h1>
+    }
+
     return (
         <h1>Profile component</h1>
     )
