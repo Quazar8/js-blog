7bb9149ed9370f38c00e9fd9b7d75140094e7aae
Quazar8 <barcode12d@gmail.com>
2021-01-03 23:25:33 +0200

    Initial markup in the profile page component

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -5,8 +5,14 @@ const Profile = ({ user }) => {
         return <h1>Can not display profile page</h1>
     }
 
+    const { profilePic, username } = user
     return (
-        <h1>Profile component</h1>
+        <section className = "profile-page">
+            <div className = "image-container">
+                <img src = { profilePic } alt = "profile picture" />
+            </div>
+            <h2>{ username }</h2>
+        </section>
     )
 }
 
