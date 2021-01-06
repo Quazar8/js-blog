cf52ca1039a58c0f3ccb6e0f4d1347bd2df95fe5
Quazar8 <barcode12d@gmail.com>
2021-01-06 18:33:01 +0200

    Copying post's markup into the input post component

--- client/components/post/InputPost.js
+++ client/components/post/InputPost.js
@@ -1,10 +1,27 @@
 import React from 'react'
 
 const InputPost = () => {
+    const { thumbnail, authorId, title, content} = {
+        thumbnail: '',
+        authorId: 'Example Author',
+        title: 'Example Title',
+        content: 'Example Content'
+    }
+
     return (
-        <div>
-            <h1>Input Post</h1>
-        </div>
+        <section className = "post-container">
+            <article className = "post-view">
+                <div className = "thumbnail-container">
+                    <img src = { thumbnail } alt = "thumbnail picture" />
+                    <div className = "shader-left"></div>
+                    <div className = "shader-bottom"></div>
+                    <div className = "shader-right"></div>
+                </div>
+                <h2>{ title }</h2>
+                <h3>Author: { authorId }</h3>
+                <p>{ content }</p>
+            </article>
+        </section>
     )
 }
 
