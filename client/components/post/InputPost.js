69efce50596a55b8c514b29c279b2d0157375fde
Quazar8 <barcode12d@gmail.com>
2021-01-06 20:10:09 +0200

    Showing thumbnail in input post on choosing an image

--- client/components/post/InputPost.js
+++ client/components/post/InputPost.js
@@ -1,23 +1,32 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const InputPost = () => {
-    const { thumbnail, authorId, title, content} = {
-        thumbnail: '',
-        authorId: 'Example Author',
-        title: 'Example Title',
-        content: 'Example Content'
+     const post = {
+        title: '',
+        content: '',
+        authorId: '',
+        thumbnail: ''
+    }
+
+    const { title, content, authorId, thumbnail } = post
+
+    const imageRef = useRef()
+    const handleImageInput = (e) => {
+        const file = e.target.files[0]
+
+        imageRef.current.src = URL.createObjectURL(file)
     }
 
     return (
         <section className = "post-container input-post">
             <article className = "post-view">
                 <div className = "thumbnail-container">
-                    <img src = { thumbnail } alt = "thumbnail picture" />
+                    <img ref = { imageRef } src = "" alt = "thumbnail picture" />
                     <div className = "shader-left"></div>
                     <div className = "shader-bottom"></div>
                     <div className = "shader-right"></div>
                     <label id = "thumbnail-label">
-                        <input type = "file" />
+                        <input onChange = { handleImageInput } type = "file" />
                         Choose a thumbnail for this post
                     </label>
                 </div>
