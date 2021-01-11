3aefc1db28d4aabb6c0b55db1da805746fe4dcfb
Quazar8 <barcode12d@gmail.com>
2021-01-11 23:32:45 +0200

    Displaying initial user information in profile view

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -16,17 +16,25 @@ const Profile = ({ match }) => {
                 return
             }
 
+            console.log(resp.user)
             setUser(resp.user)
         })
     }, [])
 
-    const { profilePic, username } = user
+    const { profilePic, username, totalPosts, posts } = user
     return (
         <section className = "profile-page">
             <div className = "image-container">
                 <img src = { profilePic } alt = "profile picture" />
             </div>
             <h2>{ username }</h2>
+            <h3>Author of { totalPosts } articles.</h3>
+            <div>
+            {
+                posts.map((p, i) => <h4 key = { i }>{ p.title }</h4>)
+                    .reverse()
+            }
+            </div>
         </section>
     )
 }
