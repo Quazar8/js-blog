41ff37ddd7d2c7f34accff963c0719f4227a4194
Quazar8 <barcode12d@gmail.com>
2021-01-15 22:20:29 +0200

    Styling the author buttons in post view

--- client/components/post/PostAuthorButtons.js
+++ client/components/post/PostAuthorButtons.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PostAuthorButtons = () => {
     return (
-        <div>
+        <div className = "button-container">
             <button>Edit</button>
             <button>Delete</button>
         </div>
