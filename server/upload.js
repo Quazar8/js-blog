4ae20b18917f421a65b25a01dd8b85309c440d4b
Quazar8 <barcode12d@gmail.com>
2021-01-22 20:59:16 +0200

    Seperate instance of multer image upload function

---
+++ server/upload.js
@@ -0,0 +1,31 @@
+const checkFileType = (file, cb) => {
+    const permitted = /jpeg|jpg|png/
+
+    const ext = path.extname(file.originalname).toLowerCase()
+    const checkExt = permitted.test(ext)
+    const checkMime = permitted.test(file.mimetype)
+
+    if (checkExt && checkMime) {
+        return cb(null, true)
+    }
+
+    return cb('File is not of an allowed type')
+}
+
+const storage = multer.diskStorage({
+    destination: './static/uploads/',
+    filename: (req, file, cb) => {
+        const name = genId(16) + 
+                        path.extname(file.originalname)
+        cb(null, name)
+    }
+})
+
+const uploadImage = multer({
+        storage,
+        fileFilter: (req, file, cb) => {
+            checkFileType(file, cb)
+        }
+})
+
+module.exports = uploadImage
