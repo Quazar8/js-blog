043613da3896d1e2b21bad8a9bc17ffc367f2f62
Quazar8 <barcode12d@gmail.com>
2021-01-24 23:54:08 +0200

    Retrieving user posts on page mount

--- client/components/user/UserPosts.js
+++ client/components/user/UserPosts.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getUserPostsServer } from '../../api'
 
 const UserPosts = () => {
+    useEffect(() => {
+        const userId = 'nick2'
+        const page = 1
+        getUserPostsServer(userId, page).then(resp => {
+            console.log(resp)
+        })
+    }, [])
+
     return (
         <div>
             This is the user post component
