ac5c4a02f08db3f522d15189b7c98daa990ed513
Quazar8 <barcode12d@gmail.com>
2021-01-25 22:49:51 +0200

    React component for displaying user post previews

---
+++ client/components/user/UserPostPreview.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const UserPostPreview = ({ post }) => {
+    const { title, content, thumbnail} = post
+    const shortContent = content.substring(0, 160)
+    return (
+        <div className = "user-post-preview">
+            <div className = "image-container">
+                <img src = "" alt = "post-thumbnail" />
+            </div>
+            <h3>{ title }</h3>
+            <p>{ shortContent }</p>
+        </div>
+    )
+}
+
+export default UserPostPreview
