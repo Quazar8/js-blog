58a4ea2d64dccd822c75bc5966014d75b37ed9f1
Quazar8 <barcode12d@gmail.com>
2021-01-25 22:58:00 +0200

    Setting state after retrieving user posts

--- client/components/user/UserPosts.js
+++ client/components/user/UserPosts.js
@@ -1,17 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { getUserPostsServer } from '../../api'
 
 const UserPosts = ({ match }) => {
+    const [posts, setPosts] = useState([])
+
     useEffect(() => {
         const { userId, pageNum } = match.params
         getUserPostsServer(userId, pageNum).then(resp => {
             console.log(resp)
+            if (resp.error) {
+                console.error(resp.errorMsg)
+                return
+            }
+
+            setPosts(resp.userPosts)
         })
     }, [])
 
     return (
-        <div>
-            This is the user post component
+        <div className = "user-posts-container">
+            {
+                posts.map((post, i) => {
+                    return <h3 key = { i }>{ post.title }</h3>
+                }).reverse()
+            }
         </div>
     )
 }
