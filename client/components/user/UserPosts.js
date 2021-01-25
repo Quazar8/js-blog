4f53415bd9ec3bb5ed88dbb8efaee980298597d5
Quazar8 <barcode12d@gmail.com>
2021-01-25 23:08:56 +0200

    Showing notification on error in user posts component

--- client/components/user/UserPosts.js
+++ client/components/user/UserPosts.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import UserPostPreview from './UserPostPreview'
+
+import { showError } from '../../store/globalActions'
 import { getUserPostsServer } from '../../api'
 
 const UserPostsView = ({ match, dispatchToStore }) => {
@@ -10,6 +12,7 @@ const UserPostsView = ({ match, dispatchToStore }) => {
         const { userId, pageNum } = match.params
         getUserPostsServer(userId, pageNum).then(resp => {
             if (resp.error) {
+                dispatchToStore(showError(resp.errorMsg))
                 console.error(resp.errorMsg)
                 return
             }
