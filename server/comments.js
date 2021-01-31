905bd0169b651f5693cf1a806ed603ed5117b2eb
Quazar8 <barcode12d@gmail.com>
2021-01-31 23:28:37 +0200

    Server side validation of the post comment request

--- server/comments.js
+++ server/comments.js
@@ -1,5 +1,22 @@
 const { successResponse, errorResponse } = require('./utils')
+
 const postComment = (req, res) => {
+    const { postId } = req.params
+    if (!postId) {
+        res.status(400).send(errorResponse({}, 'Missing post id'))
+    }
+
+    const { content } = req.body
+    if (!content) {
+        res.status(400).send(errorResponse({}, 'Empty comment'))
+    }
+
+    const db = require('./db.json')
+    const post = db.Posts[postId]
+    if (!post) {
+        res.status(400).send(errorResponse({ postId }, 'Invalid post id'))
+    }
+
     res.status(200).send(successResponse({}, 'Comment route under construction'))
 }
 
