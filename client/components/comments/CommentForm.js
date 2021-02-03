8d7f21daa20e0a0c189c6ff2a083255175bea07a
Quazar8 <barcode12d@gmail.com>
2021-02-03 23:18:18 +0200

    Comment form markup

--- client/components/comments/CommentForm.js
+++ client/components/comments/CommentForm.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const CommentForm = () => {
     return (
-        <form>
-            Write Comment
+        <form className = "comment-form">
+            <div className = "image-container">
+                <img src = "" alt = "profile-pic" />
+            </div>
+            <p contentEditable></p>
         </form>
     )
 }
