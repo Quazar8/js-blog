3dc45047fcd7358ff0a54c8b89e0ee5402abe43e
Quazar8 <barcode12d@gmail.com>
2021-02-04 22:43:26 +0200

    Creating initial react component for the comment section of the post

---
+++ client/components/comments/CommentSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CommentSection = () => {
+    return (
+        <section className = "comment-section">
+            CommentSection
+        </section>
+    )
+}
+
+export default CommentSection
