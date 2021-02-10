a25e6e306a41343435cc36cdcbbed77ef918d49c
Quazar8 <barcode12d@gmail.com>
2021-02-10 23:20:20 +0200

    Initial function to handle comment submit

--- client/components/comments/CommentForm.js
+++ client/components/comments/CommentForm.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
+import { postComment } from '../../api'
+
 const CommentForm = ({ profilePic }) => {
+    const commentSubmit = (e) => {
+        e.preventDefault()
+    }
+
+
     return (
-        <form className = "comment-form">
+        <form className = "comment-form" onSubmit = { commentSubmit }>
             <div className = "comment-body">
                 <div className = "image-container">
                     <img src = { profilePic } alt = "profile-pic" />
