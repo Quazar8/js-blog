14915360edf258f07c2031a224b9a5050ebfe513
Quazar8 <barcode12d@gmail.com>
2021-02-16 14:06:16 +0200

    A react component for editing a comment

---
+++ client/components/comments/EditCommentForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditCommentForm = () => {
+    return (
+        <form className = "edit-comment-form">
+            EditCommentForm
+        </form>
+    )
+}
+
+export default EditCommentForm
