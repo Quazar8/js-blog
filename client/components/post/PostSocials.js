9a4306be0cbac400494168eea79f4823b8a79782
Quazar8 <barcode12d@gmail.com>
2021-03-01 20:18:21 +0200

    Changing the star symbol on hover in post view

--- client/components/post/PostSocials.js
+++ client/components/post/PostSocials.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const PostSocials = () => {
+    const [star, setStar] = useState('\u2606')
+
+    const changeToFilledStar = () => {
+        setStar('\u2605')
+    }
+
+    const changeToHollowStar = () => {
+        setStar('\u2606')
+    }
+    
     return (
         <div className = "post-socials-container">
-            0 &#9734;
+            0 <span
+                className = "star"
+                onMouseEnter = { changeToFilledStar }
+                onMouseLeave = { changeToHollowStar }
+            >
+                { star }
+            </span>
         </div>
     )
 }
