206a1cdec4ae5ffa1647a4df758bee55d61c852e
Quazar8 <barcode12d@gmail.com>
2021-03-05 18:19:20 +0200

    Adding a linear gradient to the wire svg

--- client/components/svgs/Wire.js
+++ client/components/svgs/Wire.js
@@ -3,8 +3,18 @@ import React from 'react'
 const Wire = () => {
     return (
         <svg viewBox = "0 0 100 100" className = "wire">
+            <defs>
+                <linearGradient id = "grad"
+                x1 = "0%" y1 = "0%"
+                x2 = "100%" y2 = "0%">
+                    <stop className = "color1" offset = "35%"></stop>
+                    <stop className = "color2" offset = "55%"></stop>
+                    <stop className = "color3" offset = "75%"></stop>
+                </linearGradient>
+            </defs>
             <path
                 d = "M130 50 L55 50 L40 30"
+                stroke = "url(#grad)"
             />
             <circle 
                 cx = "37"
