ec054e84de576beca6678a44f04c43c0e0da4e90
Quazar8 <barcode12d@gmail.com>
2021-03-05 23:34:01 +0200

    Making the circle in the wire svg participate in the animation

--- client/components/svgs/Wire.js
+++ client/components/svgs/Wire.js
@@ -16,30 +16,68 @@ const Wire = ({ index }) => {
                 x1 = "0%" y1 = "0%"
                 x2 = "100%" y2 = "0%">
                     <stop className = "color1">
-                        <animate 
+                        <animate
+                            id = "w-c1"
                             attributeName = "offset"
                             from = "-10%"
                             to = "140%"
                             dur = "2s"
-                            repeatCount = "indefinite"
+                            begin = "1s;c1.end"
                         />
                     </stop>
                     <stop className = "color2">
                         <animate 
+                            id = "w-c2"
                             attributeName = "offset"
                             from = "0%"
                             to = "150%"
                             dur = "2s"
-                            repeatCount = "indefinite"
+                            begin = "1s;c2.end"
                         />
                     </stop>
                     <stop className = "color3">
                         <animate 
+                            id = "w-c3"
                             attributeName = "offset"
                             from = "10%"
                             to = "160%"
                             dur = "2s"
-                            repeatCount = "indefinite"
+                            begin = "1s;c3.end"
+                        />
+                    </stop>
+                </linearGradient>
+
+                <linearGradient id = "grad-circle"
+                x1 = "0%" y1 = "10%"
+                x2 = "100%" y2 = "100%">
+                    <stop className = "color1">
+                        <animate 
+                            id = "c1"
+                            attributeName = "offset"
+                            from = "-10%"
+                            to = "80%"
+                            dur = "1s"
+                            begin = "0s; w\-c1.end"
+                        />
+                    </stop>
+                    <stop className = "color2">
+                        <animate 
+                            id = "c2"
+                            attributeName = "offset"
+                            from = "0%"
+                            to = "90%"
+                            dur = "1s"
+                            begin = "0s;w\-c2.end"
+                        />
+                    </stop>
+                    <stop className = "color3">
+                        <animate 
+                            id = "c3"
+                            attributeName = "offset"
+                            from = "10%"
+                            to = "100%"
+                            dur = "1s"
+                            begin = "0s;w\-c3.end"
                         />
                     </stop>
                 </linearGradient>
@@ -52,6 +90,7 @@ const Wire = ({ index }) => {
                 cx = "37"
                 cy = "25"
                 r = "7"
+                stroke = "url(#grad-circle)"
             />
         </svg>
     )
