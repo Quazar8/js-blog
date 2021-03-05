8b876a9ae75a675c41b5bb71263766e459169eae
Quazar8 <barcode12d@gmail.com>
2021-03-05 19:55:07 +0200

    Animating the gradient of the wire svg

--- client/components/svgs/Wire.js
+++ client/components/svgs/Wire.js
@@ -7,9 +7,33 @@ const Wire = () => {
                 <linearGradient id = "grad"
                 x1 = "0%" y1 = "0%"
                 x2 = "100%" y2 = "0%">
-                    <stop className = "color1" offset = "35%"></stop>
-                    <stop className = "color2" offset = "55%"></stop>
-                    <stop className = "color3" offset = "75%"></stop>
+                    <stop className = "color1">
+                        <animate 
+                            attributeName = "offset"
+                            from = "-10%"
+                            to = "140%"
+                            dur = "2s"
+                            repeatCount = "indefinite"
+                        />
+                    </stop>
+                    <stop className = "color2">
+                        <animate 
+                            attributeName = "offset"
+                            from = "0%"
+                            to = "150%"
+                            dur = "2s"
+                            repeatCount = "indefinite"
+                        />
+                    </stop>
+                    <stop className = "color3">
+                        <animate 
+                            attributeName = "offset"
+                            from = "10%"
+                            to = "160%"
+                            dur = "2s"
+                            repeatCount = "indefinite"
+                        />
+                    </stop>
                 </linearGradient>
             </defs>
             <path
